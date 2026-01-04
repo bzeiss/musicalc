2eb844a62fe9c318d9d69af132c26e9356778681
B. Zeiss <ben@zeiss.net>
2026-01-04 12:14:30 +0100

    reworked tempo to delay UI

--- internal/ui/tempotodelay.go
+++ internal/ui/tempotodelay.go
@@ -10,11 +10,59 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+// responsiveTable wraps a table and updates column widths on resize
+type responsiveTable struct {
+	widget.BaseWidget
+	table             *widget.Table
+	updateColumnWidth func(float32)
+}
+
+func newResponsiveTable(table *widget.Table, updateFunc func(float32)) *responsiveTable {
+	r := &responsiveTable{
+		table:             table,
+		updateColumnWidth: updateFunc,
+	}
+	r.ExtendBaseWidget(r)
+	return r
+}
+
+func (r *responsiveTable) CreateRenderer() fyne.WidgetRenderer {
+	return &responsiveTableRenderer{
+		responsive: r,
+		table:      r.table,
+	}
+}
+
+type responsiveTableRenderer struct {
+	responsive *responsiveTable
+	table      *widget.Table
+}
+
+func (r *responsiveTableRenderer) Layout(size fyne.Size) {
+	r.table.Resize(size)
+	r.responsive.updateColumnWidth(size.Width)
+}
+
+func (r *responsiveTableRenderer) MinSize() fyne.Size {
+	return r.table.MinSize()
+}
+
+func (r *responsiveTableRenderer) Refresh() {
+	r.table.Refresh()
+}
+
+func (r *responsiveTableRenderer) Objects() []fyne.CanvasObject {
+	return []fyne.CanvasObject{r.table}
+}
+
+func (r *responsiveTableRenderer) Destroy() {}
+
 func NewTempoTab() fyne.CanvasObject {
 	bpm := binding.NewString()
-	_ = bpm.Set("120.00")
+	_ = bpm.Set("120")
 	input := widget.NewEntry()
-	input.SetText("120.00")
+	input.SetText("120")
+	input.PlaceHolder = "Tempo"
 	input.OnChanged = func(s string) {
 		_ = bpm.Set(s)
 	}
@@ -98,18 +146,40 @@ func NewTempoTab() fyne.CanvasObject {
 	// Hide row header column
 	table.ShowHeaderColumn = false
 
-	table.SetColumnWidth(0, 140)
-	table.SetColumnWidth(1, 140)
-	table.SetColumnWidth(2, 140)
+	// Set proportional column widths dynamically based on container width
+	// Using 20%, 40%, 40% distribution
+	updateColumnWidths := func(width float32) {
+		// Reserve some space for padding and separators
+		availableWidth := width - 20 // Account for padding
+		if availableWidth < 300 {
+			availableWidth = 300 // Minimum width
+		}
+
+		col0Width := availableWidth * 0.20
+		col1Width := availableWidth * 0.40
+		col2Width := availableWidth * 0.40
+
+		table.SetColumnWidth(0, col0Width)
+		table.SetColumnWidth(1, col1Width)
+		table.SetColumnWidth(2, col2Width)
+	}
+
+	// Set initial widths
+	updateColumnWidths(450)
 
 	bpm.AddListener(binding.NewDataListener(func() { table.Refresh() }))
 
+	// Wrap table in responsive container that updates widths on resize
+	responsiveTableWidget := newResponsiveTable(table, updateColumnWidths)
+
 	return container.NewBorder(
 		container.NewVBox(
+			widget.NewLabelWithStyle("Tempo to Delay", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+			widget.NewSeparator(),
 			input,
 			widget.NewSeparator(),
 		),
 		nil, nil, nil,
-		table,
+		responsiveTableWidget,
 	)
 }
