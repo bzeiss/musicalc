140e3b0cf677d9fe89a034ddecc2e3603d6ed16e
B. Zeiss <ben@zeiss.net>
2026-05-01 08:20:28 +0200

    build fixes.

--- build-win.ps1
+++ build-win.ps1
@@ -1,4 +1,9 @@
-$env:CGO_FLAGS="-O3 -flto=auto -march=x86-64-v3"
-$env:CGO_LDFLAGS="-O3 -flto=auto"
-$env:CGO_ENABLED=1
-go build -ldflags="-s -w" -o musicalc.exe
+$ErrorActionPreference = "Stop"
+
+$env:CGO_ENABLED = "1"
+$env:CC = "clang --target=x86_64-w64-windows-gnu"
+$env:CXX = "clang++ --target=x86_64-w64-windows-gnu"
+$env:CGO_CFLAGS = "-O3 -march=x86-64-v3"
+$env:CGO_LDFLAGS = "-O3"
+
+go build -ldflags="-s -w -H=windowsgui -extld=x86_64-w64-mingw32-gcc" -o musicalc.exe
